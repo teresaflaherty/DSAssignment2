3d4469dda12a84392eda82227278dab80311ce76
Gabriel-Denys <gabriel@hoolie.app>
2021-04-21 14:53:43 +0100

    Added a simple message driven bean test

---
+++ tenner/src/java/myBean/ReadMessageMDB.java
@@ -0,0 +1,30 @@
+package myBean;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(activationConfig = { 
+    @ActivationConfigProperty(
+      propertyName = "destination", 
+      propertyValue = "tutorialQueue"), 
+    @ActivationConfigProperty(
+      propertyName = "destinationType", 
+      propertyValue = "javax.jms.Queue")
+})
+public class ReadMessageMDB implements MessageListener {
+
+    @Override
+    public void onMessage(Message message) {
+        TextMessage textMessage = (TextMessage) message;
+        try {
+            System.out.println("Message received: " + textMessage.getText());
+        } catch (JMSException e) {
+            System.out.println(
+              "Error while trying to consume messages: " + e.getMessage());
+        }
+    }
+}
